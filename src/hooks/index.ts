a0bf3f52e353cf3ded903c4f7a342f8635dd2f91
Matgenius04 <Matgenius04@gmail.com>
2021-05-18 20:23:55 -0500

    upload api keys because stupid

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,8 @@
 import cookie from 'cookie'
 import { SupabaseClient } from '@supabase/supabase-js';
-import config from 'config';
 
-const SUPABASE_URL = config.get('supabase.url');
-const SUPABASE_SERVICE_KEY = config.get('supabase.serviceKey')
+const SUPABASE_URL = "https://vqqjrxggizczqfdfthzl.supabase.co";
+const SUPABASE_SERVICE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjIwMDUxMzYwLCJleHAiOjE5MzU2MjczNjB9.qWQY2E5ZHZsypQ-mMpiTHa0dYmTVcsn0D2_ptZ8wbYc";
 
 const supabase = new SupabaseClient (
   SUPABASE_URL,
