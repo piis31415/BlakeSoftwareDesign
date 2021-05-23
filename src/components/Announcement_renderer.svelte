afc72375e1dd045711842a57a280432d1a3de15a
Matthew Chen <Matgenius04@gmail.com>
2021-05-22 19:50:22 -0500

    Things actually kinda work

---
+++ src/components/Announcement_renderer.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import renderer from 'editorjs-html';
+  export let announcements;
+  const parser = renderer();
+  console.log(parser);
+  console.log(announcements)
+</script>
+
+<div class="container">
+  {#if announcements}
+    {#each announcements as announcement}
+      <div>
+      {#each JSON.parse(announcement.stored_data) as block}
+        {@html block? parser.parseBlock(block):null}
+      {/each}
+      </div>
+    {/each}
+  {/if}
+</div>
+
+<style style lang="postcss">
+  .container {
+    @apply space-y-4;
+  }
+  .container > div {
+    @apply p-4 bg-gray-100 border-highlight-blue rounded-lg;
+  }
+</style>
